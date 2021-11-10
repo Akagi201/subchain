a4f9ac1d4233c6465b92cc31c8ea9238962e3e26
Akagi201 <akagi201@gmail.com>
2021-11-10 18:10:01 +0800

    feat(poe): add poe pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -43,6 +43,9 @@ pub use sp_runtime::{Perbill, Permill};
 /// Import the template pallet.
 pub use pallet_template;
 
+/// Import the poe pallet.
+pub use pallet_poe;
+
 /// An index to a block.
 pub type BlockNumber = u32;
 
@@ -279,6 +282,11 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+/// Configure the pallet-poe in pallets/poe.
+impl pallet_poe::Config for Runtime {
+	type Event = Event;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -296,6 +304,8 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		// Include the poe logic from the pallet-poe in the runtime.
+		PoeModule: pallet_poe,
 	}
 );
 
