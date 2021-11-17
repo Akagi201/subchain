0671a8ae62387067034f5ae080814120c216ba32
Akagi201 <akagi201@gmail.com>
2021-11-17 17:14:18 +0800

    feat(nicks): add nicks pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -242,21 +242,64 @@ impl pallet_timestamp::Config for Runtime {
 
 parameter_types! {
 	pub const ExistentialDeposit: u128 = 500;
+	// A heuristic that is used for weight estimation.
 	pub const MaxLocks: u32 = 50;
 }
 
 impl pallet_balances::Config for Runtime {
+	// The previously defined parameter_type is used as a configuration parameter.
 	type MaxLocks = MaxLocks;
 	type MaxReserves = ();
 	type ReserveIdentifier = [u8; 8];
 	/// The type for recording an account's balance.
+	/// The "Balance" that appears after the equal sign is an alias for the u128 type.
 	type Balance = Balance;
 	/// The ubiquitous event type.
 	type Event = Event;
+	// The empty value, (), is used to specify a no-op callback function.
 	type DustRemoval = ();
+	// The previously defined parameter_type is used as a configuration parameter.
 	type ExistentialDeposit = ExistentialDeposit;
+	// The FRAME runtime system is used to track the accounts that hold balances.
 	type AccountStore = System;
+	// Weight information is supplied to the Balances pallet by the node template runtime.
+	// type WeightInfo = (); // old way
 	type WeightInfo = pallet_balances::weights::SubstrateWeight<Runtime>;
+}
+
+// Add this code block to your template for Nicks:
+parameter_types! {
+	// Choose a fee that incentivizes desireable behavior.
+	pub const NickReservationFee: u128 = 100;
+	pub const MinNickLength: u32 = 8;
+	// Maximum bounds on storage are important to secure your chain.
+	pub const MaxNickLength: u32 = 32;
+}
+
+impl pallet_nicks::Config for Runtime {
+	// The Balances pallet implements the ReservableCurrency trait.
+	// `Balances` is defined in `construct_runtime!` macro. See below.
+	// https://docs.substrate.io/rustdocs/latest/pallet_balances/index.html#implementations-2
+	type Currency = Balances;
+
+	// Use the NickReservationFee from the parameter_types block.
+	type ReservationFee = NickReservationFee;
+
+	// No action is taken when deposits are forfeited.
+	type Slashed = ();
+
+	// Configure the FRAME System Root origin as the Nick pallet admin.
+	// https://docs.substrate.io/rustdocs/latest/frame_system/enum.RawOrigin.html#variant.Root
+	type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+
+	// Use the MinNickLength from the parameter_types block.
+	type MinLength = MinNickLength;
+
+	// Use the MaxNickLength from the parameter_types block.
+	type MaxLength = MaxNickLength;
+
+	// The ubiquitous event type.
+	type Event = Event;
 }
 
 parameter_types! {
@@ -306,6 +349,7 @@ construct_runtime!(
 		TemplateModule: pallet_template,
 		// Include the poe logic from the pallet-poe in the runtime.
 		PoeModule: pallet_poe,
+		Nicks: pallet_nicks,
 	}
 );
 
